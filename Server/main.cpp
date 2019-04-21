0950c10d8e3ae06401566b87912f36b86d302443
Jimin  Mun <jmmun@UCSD.EDU>
2019-04-21 15:00:32 -0700

    server loop for listening multiple clients

--- Server/main.cpp
+++ Server/main.cpp
@@ -22,19 +22,26 @@ void send_(tcp::socket & socket, const string& message) {
 }
 
 int server() {
-	boost::asio::io_service io_service;
-	//listen for new connection
-	tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 1234));
-	//socket creation 
-	tcp::socket socket_(io_service);
-	//waiting for connection
-	acceptor_.accept(socket_);
-	//read operation
-	string message = read_(socket_);
-	cout << message << endl;
-	//write operation
-	send_(socket_, "Hello From Server!");
-	cout << "Servent sent Hello message to Client!" << endl;
+	try {
+		boost::asio::io_service io_service;
+		//listen for new connection
+		tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 1234));
+		for (;;) {
+			//socket creation 
+			tcp::socket socket_(io_service);
+			//waiting for connection
+			acceptor_.accept(socket_);
+			//read operation
+			string message = read_(socket_);
+			cout << message << endl;
+			//write operation
+			send_(socket_, "Hello From Server!");
+			cout << "Servent sent Hello message to Client!" << endl;
+		}
+	}
+	catch (std::exception& e){
+		std::cerr << e.what() << std::endl;
+	}
 	return 0;
 }
 
