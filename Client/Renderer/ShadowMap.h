71ab5f720269783445586fc1a116307c5d9efb55
Brian Hang <hangwithbrian@gmail.com>
2019-04-21 20:34:16 -0700

    Add ShadowMap class

---
+++ Client/Renderer/ShadowMap.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <GL/glew.h>
+#include "Texture.h"
+#include "Shader.h"
+#include "DirectionalLight.h"
+
+// The ShadowMap class sets up a depth map that stores depths from a light
+// for use in calculating shadows.
+class ShadowMap {
+	private:
+	int width; // Width of the depth map.
+	int height; // Height of the depth map.
+	int viewport[4]; // Info about viewport before rendering to depth map.
+	GLuint FBO; // ID for depth map framebuffer.
+	Texture depthMap; // Texture that stores depth of vertices from a light's POV.
+	Shader shadowShader; // Shader for drawing to depth map.
+
+	public:
+	ShadowMap(int width = 1024, int height = 1024);
+
+	// Does some OpenGL setup before rendering the scene for the shadow pass.
+	void prePass();
+
+	// Cleans up the set up stuff after rendering the scene for the shadow pass.
+	void postPass();
+
+	// Sets texture 31 to be the depth map - used in lighting shader.
+	void bindTexture(Shader &shader) const;
+
+	// Sets the light that will be used to create the depth map.
+	void setupLight(Shader &shader, const DirectionalLight &light);
+
+	// Returns the shadow shader.
+	Shader &getShader();
+
+	~ShadowMap();
+};
