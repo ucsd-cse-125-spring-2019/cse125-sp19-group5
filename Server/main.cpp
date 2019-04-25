e11fa895661e771d8ab6ecf2050812429b818b63
Jimin  Mun <jmmun@UCSD.EDU>
2019-04-25 10:51:46 -0700

    changes to pointer for sockets

--- Server/main.cpp
+++ Server/main.cpp
@@ -9,6 +9,15 @@ using std::string;
 using std::cout;
 using std::endl;
 
+//typedef boost::shared_ptr<tcp::socket> socket_ptr;
+//
+//class session{
+//session(boost::asio::io_service& io_service)
+//	: socket_(io_service)
+//{
+//}
+//};
+
 string read_(tcp::socket & socket) {
 	boost::asio::streambuf buf;
 	boost::asio::read_until(socket, buf, "\n");
@@ -21,6 +30,8 @@ void send_(tcp::socket & socket, const string& message) {
 	boost::asio::write(socket, boost::asio::buffer(message));
 }
 
+
+
 int server() {
 	try {
 		game_message msg;
@@ -28,12 +39,20 @@ int server() {
 		boost::system::error_code err;
 		//listen for new connection
 		tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 1234));
-					//socket creation 
-			tcp::socket socket_(io_service);
 
-			//waiting for connection
-			acceptor_.accept(socket_);
-			cout << "Accept Returned" << endl;
+		/*for (;;)
+		{
+			socket_ptr sock(new tcp::socket(io_service));
+			acceptor_.async_accept(*sock);
+			boost::thread t(boost::bind(session, sock));
+		}*/
+		//socket creation 
+		tcp::socket socket_(io_service);
+
+		//waiting for connection
+		acceptor_.accept(socket_);
+		cout << "Accept Returned" << endl;
+
 		for (;;) {
 
 			//read operation
