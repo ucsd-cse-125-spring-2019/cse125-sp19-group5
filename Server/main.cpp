324e1a13d1bc821e5f2a1c0b603ed078724795cc
Jose P Valle <jpvalle@ucsd.edu>
2019-04-25 23:59:28 -0700

    The server can now broadcast game state to each connected client

--- Server/main.cpp
+++ Server/main.cpp
@@ -12,17 +12,18 @@ using boost::asio::ip::tcp;
 boost::asio::io_service ioService;
 tcp::endpoint endpoint{ tcp::v4(), 1234 };
 tcp::acceptor acceptor{ ioService, endpoint };
-std::vector<tcp::socket*> sockets;
+std::vector<tcp::socket*> sockets;		// list of all connected sockets
 tcp::socket *newSocket = nullptr;
 
-constexpr auto TICKS_PER_SECOND = 60;
+constexpr auto TICKS_PER_SECOND = 60;//HELP: what number should this be? 
 
 char messageData[128];
+boost::asio::streambuf buffer;
 
 void receiveHandler(std::error_code ec, std::size_t bytes) {
 	std::cout << ec.message() << std::endl;
 	std::cout << "Received message of size " << bytes << std::endl;
-		std::string blah;
+	std::string blah;
 }
 
 void handleIncomingConnections();
@@ -38,12 +39,17 @@ void onReceiveMessageHeader(
 		return;
 	}
 
-	std::cout << "Received " << bytes << " bytes long message" << std::endl;
+	std::istream is(&buffer);
+	std::string s;
+
+	//s is the message that was sent
+	is >> s;
+
+	std::cout << s << std::endl;
 	readMessageHeader(socket);
 }
 
 void readMessageHeader(tcp::socket *socket) {
-	boost::asio::streambuf buffer;
 	/*
 	// TODO (bhang): change this to support a header size
 	socket->async_read_some(
@@ -53,26 +59,35 @@ void readMessageHeader(tcp::socket *socket) {
 		}
 	);
 	*/
+
 	boost::asio::async_read_until(
 		*socket,
 		buffer,
 		"\n",
-		[socket](const boost::system::error_code &error, std::size_t bytes) {
-			onReceiveMessageHeader(socket, error, bytes);
+		[socket](const boost::system::error_code &error, std::size_t bytes) 
+		{
+		onReceiveMessageHeader(socket, error, bytes);
 		}
 	);
 }
 
 void onAccept(const std::error_code& error) {
-	if (!error) {
-		std::cout << "A client has connected!" << std::endl;
-	} else {
-		std::cout << error.message() << std::endl;
+	if (error) 
+	{
+		std::cout << "AN ERROR OCCURRED: " << error.message() << std::endl;
 	}
 
+	//TODO: Assign an player ID to the client and send it to client
+
+	//FUTURE: read the IP and Port and map to an ID
+
+	//read the message that came in
 	readMessageHeader(newSocket);
 
+	//list of currently added sockets
 	sockets.push_back(newSocket);
+
+	//keep listening (this is done recursive)
 	handleIncomingConnections();
 }
 
@@ -98,6 +113,43 @@ int main(int argc, char **argv) {
 		for (int i = 0; i < 4; i++) {
 			gameEngine.updateGameState(playerInputs);
 		}
+
+		/* NOTE:  
+		 * ----
+		 * Here is how we figured out how to send (broadcast) 
+		 * the game state data out to each of the clients. We 
+		 * have not considered client disconnects or anything 
+		 * like that. Not even sure if asynchronous send is 
+		 * appropriate considering the server is on a synchronous
+		 * loop to begin with. 
+		 */
+
+		//broadcast to each connected client
+		for (auto client : sockets) {
+
+			/* -------
+			 * WARNING
+			 * -------
+			 * Sending spaces in data doesn't seem to work.  
+			 * The client cuts off the message before the space.
+			 */
+			//This is the message
+			const char* data = "GAME STATE!";
+			size_t data_length = 11;
+
+			/* -------
+			 * WARNING
+			 * -------
+			 * C++ string doesn't play nice with boost::asio::buffer 
+			 * Use the char* instead! 
+			 */
+			client->async_send(boost::asio::buffer(data, data_length),[](const boost::system::error_code& ec,
+				std::size_t bytes_transferred)
+			{
+				/*TODO: handle errors*/
+			});
+		}
+
 		std::this_thread::sleep_for(
 			std::chrono::milliseconds(1000 / TICKS_PER_SECOND)
 		);
