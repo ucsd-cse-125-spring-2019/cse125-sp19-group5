ce946c48de423e1a5ed4b1afd487145005255479
Keenan Mach <kmach@UCSD.EDU>
2019-05-02 11:42:23 -0700

    fixed commented server/main code

--- Server/main.cpp
+++ Server/main.cpp
@@ -10,66 +10,66 @@
 constexpr auto TICKS_PER_SECOND = 60; // How many updates per second.
 
 int main(int argc, char **argv) {
-	//Network::init(1234);
+	Network::init(1234);
 
-	//GameEngine gameEngine;
-	//GameStateNet gameState;
-	//auto origin = vec3(0.0f);
-	//gameState.gameObjects.push_back(Player(origin, origin, origin, "idk", 1));
-	//gameState.in_progress = false;
-	//gameState.score = std::make_tuple(1, 2);
-	//gameState.timeLeft = 30;
+	GameEngine gameEngine;
+	GameStateNet gameState;
+	auto origin = vec3(0.0f);
+	gameState.gameObjects.push_back(Player(origin, origin, origin, 0, 1));
+	gameState.in_progress = false;
+	gameState.score = std::make_tuple(1, 2);
+	gameState.timeLeft = 30;
 
-	//float ballX = 0.0f;
+	float ballX = 0.0f;
 
-	//// Handle player requests to move the ball.
-	//auto handleBallMove = [&](Connection *c, NetBuffer &buffer) {
-	//	ballX += buffer.read<float>();
+	// Handle player requests to move the ball.
+	auto handleBallMove = [&](Connection *c, NetBuffer &buffer) {
+		ballX += buffer.read<float>();
 
-	//	// Replicate the changes so everyone sees the update.
-	//	NetBuffer updateBuffer(NetMessage::BALL_X);
-	//	updateBuffer.write<float>(ballX);
-	//	Network::broadcast(updateBuffer);
-	//};
+		// Replicate the changes so everyone sees the update.
+		NetBuffer updateBuffer(NetMessage::BALL_X);
+		updateBuffer.write<float>(ballX);
+		Network::broadcast(updateBuffer);
+	};
 
-	//Network::onClientConnected([&ballX, &handleBallMove](Connection *c) {
-	//	std::cout << "Player " << c->getId() << " has connected." << std::endl;
+	Network::onClientConnected([&ballX, &handleBallMove](Connection *c) {
+		std::cout << "Player " << c->getId() << " has connected." << std::endl;
 
-	//	// Sync up the current state of ballX.
-	//	NetBuffer buffer(NetMessage::BALL_X);
-	//	buffer.write<float>(ballX);
-	//	c->send(buffer);
+		// Sync up the current state of ballX.
+		NetBuffer buffer(NetMessage::BALL_X);
+		buffer.write<float>(ballX);
+		c->send(buffer);
 
-	//	// Allow the newly connected player to move the ball.
-	//	c->on(NetMessage::BALL_X, handleBallMove);
+		// Allow the newly connected player to move the ball.
+		c->on(NetMessage::BALL_X, handleBallMove);
 
-	//	c->onDisconnected([](Connection *c) {
-	//		std::cout << "Player " << c->getId() << " has disconnected."
-	//			<< std::endl;
-	//	});
-	//});
+		c->onDisconnected([](Connection *c) {
+			std::cout << "Player " << c->getId() << " has disconnected."
+				<< std::endl;
+		});
+	});
 
-	//while (true) {
-	//	Network::poll();
+	while (true) {
+		Network::poll();
 
-	//	vector<PlayerInputs> playerInputs;
-	//	for (int i = 0; i < 4; i++) {
-	//		gameEngine.updateGameState(playerInputs);
-	//	}
+		vector<PlayerInputs> playerInputs;
+		for (int i = 0; i < 4; i++) {
+			gameEngine.updateGameState(playerInputs);
+		}
 
-	//	gameState.timeLeft -= 1;
+		gameState.timeLeft -= 1;
 
-	//	Network::broadcast(NetMessage::TEST, gameState);
+		Network::broadcast(NetMessage::TEST, gameState);
 
-	//	std::this_thread::sleep_for(
-	//		std::chrono::milliseconds(1000 / TICKS_PER_SECOND)
-	//	);
-	//}
+		std::this_thread::sleep_for(
+			std::chrono::milliseconds(1000 / TICKS_PER_SECOND)
+		);
+	}
 
 
 
 	// testing code
-	std::cout << "Hello world!" << std::endl;
+	/*std::cout << "Hello world!" << std::endl;
 	GameEngine gameEngine;
 	gameEngine.addGameObject(new Player(vec3(-2, 0, 0), vec3(1, 0, 0), vec3(1, 0, 0), 0, 1));
 	gameEngine.addGameObject(new Ball(vec3(5, 0, 0), vec3(-1, 0, 0), 0, 1));
@@ -95,7 +95,7 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	system("pause");
+	system("pause");*/
 
 	return 0;
 }
