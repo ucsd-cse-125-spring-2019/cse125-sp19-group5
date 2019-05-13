dbe6afa185688246d428450cc5ef445f3a25ce57
Brian Hang <hangwithbrian@gmail.com>
2019-05-13 13:52:31 -0700

    Add missing GameObject constructors to game objects

--- Shared/Shared/Ball.h
+++ Shared/Shared/Ball.h
@@ -3,6 +3,8 @@
 
 class Ball : public GameObject {
 public:
+	using GameObject::GameObject;
+
 	Ball(vec3 position, vec3 velocity, int id, int radius);
 	GAMEOBJECT_TYPES getGameObjectType() const;
 	void onCollision(GameObject * gameObject);
--- Shared/Shared/Wall.h
+++ Shared/Shared/Wall.h
@@ -3,6 +3,8 @@
 
 class Wall : public GameObject {
 public:
+	using GameObject::GameObject;
+
 	Wall(vec3 position, vec3 velocity, int id, int radius);
 	GAMEOBJECT_TYPES getGameObjectType() const;
 };
