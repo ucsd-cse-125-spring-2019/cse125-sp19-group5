05b7e8fe23a7785140414e828df642d1200b0572
Keenan Mach <kmach@UCSD.EDU>
2019-05-30 17:03:34 -0700

    prevent gravity being applied on balls when theyre grounded

--- Shared/Shared/Ball.cpp
+++ Shared/Shared/Ball.cpp
@@ -12,7 +12,7 @@ GAMEOBJECT_TYPES Ball::getGameObjectType() const {
 }
 
 void Ball::updateOnServerTick() {
-	if (glm::length(getVelocity()) > 0) { 
+	if (glm::length(getVelocity()) > 0) {
 		vec3 direction = glm::normalize(getVelocity());
 		vec3 updatedVelocity = getVelocity() - (direction * 0.01f);
 		// vec3 updatedVelocity = getVelocity() * 0.99f;
@@ -35,7 +35,10 @@ void Ball::updateOnServerTick() {
 		}
 	}
 
-	setVelocity(PhysicsEngine::applyGravity(getVelocity(), PhysicsEngine::getGravity()));
+	if (!this->isGrounded) {
+		this->ticksSinceGrounded += 1;
+		setVelocity(PhysicsEngine::applyGravity(getVelocity(), PhysicsEngine::getGravity()));
+	}
 }
 
 bool Ball::getGoalScored() {
@@ -77,12 +80,6 @@ void Ball::onCollision(Ball * ball) {
 
 			setVelocity(newVelocity);
 			ball->setVelocity(ballNewVelocity);
-
-			/*float intersectDist = getBoundingSphere()->getRadius() + ball->getBoundingSphere()->getRadius() - distanceFrom(ball);
-			while (collidesWith(ball)) {
-				move(glm::normalize(newVelocity));
-				ball->move(glm::normalize(ballNewVelocity));
-			}*/
 		}
 	}
 }
@@ -106,6 +103,8 @@ void Ball::onCollision(Paddle * paddle) {
 		paddle->getObjectsHit().insert(this);
 		currentBallCollisions.clear();
 	}
+
+	this->isGrounded = false;
 }
 
 
@@ -123,6 +122,18 @@ void Ball::onCollision(Wall * wall) {
 
 	vector<Plane * > collisionPlanes;
 	for (Plane * p : CollisionDetection::getIntersectingPlanes(this->getBoundingSphere(), wall->getBoundingBox())) {
+		if (p == wall->getBoundingBox()->top) {
+			if (!this->isGrounded) {
+				if (this->ticksSinceGrounded < 30) {
+					isGrounded = true;
+					this->velocity.y = 0;
+					vec3 adjustedPos = getPosition();
+					adjustedPos.y = wall->getPosition().y + wall->getBoundingBox()->height + getBoundingSphere()->getRadius();
+					setPosition(adjustedPos);
+				}
+			}
+			this->ticksSinceGrounded = 0;
+		}
 		vec3 planeNormal = glm::normalize(p->getNormal());
 		float angleBetween = glm::angle(glm::normalize(getVelocity()), planeNormal);
 		if (angleBetween > glm::half_pi<float>() && angleBetween < (3.0f * glm::half_pi<float>())) {
